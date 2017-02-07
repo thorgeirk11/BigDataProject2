d2aac00402d01a40208b4cceafce8d9374c96a61
centos <centos@localhost.localdomain>
2017-02-07 16:25:58 -0500

    Reading all files now workds

--- src/main.scala
+++ src/main.scala
@@ -7,13 +7,17 @@ object BigDataProject2 {
   def main(args: Array[String]): Unit = {
 
     println("Hello World")
-    println( args.length)
+    println(args.length)
 
     val conf = new SparkConf()
       .setMaster(args(0))
       .setAppName("BigDataProject2")
       .setSparkHome(args(1))
-      .setJars(SparkContext.jarOfObject(this).toSeq)
+      .setJars(SparkContext.jarOfObject(this).toSeq);
 
+    val sc = new SparkContext(conf);
+    println(sc.getConf.toDebugString);
+    val textfiles = sc.wholeTextFiles("DataCSV");
+    textfiles.foreach(println)
   }
 }
