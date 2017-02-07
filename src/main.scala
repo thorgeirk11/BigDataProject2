89611d3dbf2a1c86639931dfb40df82ea8f067ff
ingibergur <ingibergur90@gmail.com>
2017-02-07 18:54:50 +0000

    Created spark project. Hope it works for you guys :D

---
+++ src/main.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
+
+object BigDataProject2 {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World")
+    println( args.length)
+
+    val conf = new SparkConf()
+      .setMaster(args(0))
+      .setAppName("BigDataProject2")
+      .setSparkHome(args(1))
+      .setJars(SparkContext.jarOfObject(this).toSeq)
+
+  }
+}
